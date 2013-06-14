7346a39381f811428c73b51b141aa350778acf7c
anromeo <anthonykzullo@gmail.com>
2013-06-13 19:27:32 -0700

    Initial commit before berkshire

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '566e9d6b8e29d7daef0bdd723763731a85fc62ff66208d0480013a183041b453815554cfedbbbb96706264d96c984dc01aa6f9a53aaf86c15d71b541e9218a53'
